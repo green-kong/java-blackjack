0f7bc5a8f702cfe78dc390a80b4e8bac1013702e
dev_kong <ahdjd5@gmail.com>
2023-04-03 14:35:48 +0900

    feat: 이름을 생성한다.

---
+++ src/main/java/participants/Name.java
@@ -0,0 +1,42 @@
+package participants;
+
+import java.util.Objects;
+
+public class Name {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String NAME_ERROR_MSG = "1글자 이상 5글자 이하의 이름만 가능합니다.";
+    private final String value;
+
+    public Name(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException(NAME_ERROR_MSG);
+        }
+
+        if (value.length() < MIN_NAME_LENGTH || value.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(NAME_ERROR_MSG);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
